4462450dcdf0c91b12c3c06718b9d17c2a40e488
Björn Lindqvist <bjourne@gmail.com>
2021-03-30 19:58:11 +0200

    reducing the nr of clips per survey

--- musicsurvey/settings.py
+++ musicsurvey/settings.py
@@ -2,13 +2,13 @@ from django.conf import settings
 
 # Number of pairs each survey respondent has to rate.
 MUSICSURVEY_PAIRS_PER_SURVEY = \
-    getattr(settings, 'MUSICSURVEY_CLIPS_PER_SURVEY', 8)
+    getattr(settings, 'MUSICSURVEY_CLIPS_PER_SURVEY', 6)
 
 # Number of pairs per survey that contain one random clip. Used for
 # control.
 MUSICSURVEY_RANDOM_PAIRS_PER_SURVEY = \
-    getattr(settings, 'MUSICSURVEY_RANDOM_CLIPS_PER_SURVEY', 1)
+    getattr(settings, 'MUSICSURVEY_RANDOM_CLIPS_PER_SURVEY', 0)
 
 # Number of pairs per survey that contains a human-composed clip.
 MUSICSURVEY_HUMAN_PAIRS_PER_SURVEY = \
-    getattr(settings, 'MUSICSURVEY_HUMAN_PAIRS_PER_SURVEY', 4)
+    getattr(settings, 'MUSICSURVEY_HUMAN_PAIRS_PER_SURVEY', 3)
