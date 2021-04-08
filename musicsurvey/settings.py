0107b773b17cd0096f3fea498bdd06550769f65b
Björn Lindqvist <bjourne@gmail.com>
2021-04-08 18:13:26 +0200

    6 pairs were probably excessive, reducing the number to two

--- musicsurvey/settings.py
+++ musicsurvey/settings.py
@@ -2,7 +2,7 @@ from django.conf import settings
 
 # Number of pairs each survey respondent has to rate.
 MUSICSURVEY_PAIRS_PER_SURVEY = \
-    getattr(settings, 'MUSICSURVEY_CLIPS_PER_SURVEY', 6)
+    getattr(settings, 'MUSICSURVEY_CLIPS_PER_SURVEY', 2)
 
 # Number of pairs per survey that contain one random clip. Used for
 # control.
@@ -11,4 +11,4 @@ MUSICSURVEY_RANDOM_PAIRS_PER_SURVEY = \
 
 # Number of pairs per survey that contains a human-composed clip.
 MUSICSURVEY_HUMAN_PAIRS_PER_SURVEY = \
-    getattr(settings, 'MUSICSURVEY_HUMAN_PAIRS_PER_SURVEY', 3)
+    getattr(settings, 'MUSICSURVEY_HUMAN_PAIRS_PER_SURVEY', 1)
